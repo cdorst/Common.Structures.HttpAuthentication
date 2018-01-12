908a0ce65d7505a902257566028caa4466a07fd6
Christopher Dorst <chris@dorst.email>
2018-01-12 11:18:23 -0600

    Add HttpAuthentication

---
+++ Common.Structures.HttpAuthentication/IHttpAuthentication.cs
@@ -0,0 +1,9 @@
+﻿using System.Net.Http.Headers;
+
+namespace Common.Structures.HttpAuthentication
+{
+    public interface IHttpAuthentication
+    {
+        AuthenticationHeaderValue Header { get; }
+    }
+}

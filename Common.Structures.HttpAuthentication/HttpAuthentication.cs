005358de0d6a16da6d8d27f5d008e89d7fd9cea4
Christopher Dorst <chris@dorst.email>
2018-01-12 16:58:02 -0600

    Add protobuf contract

--- Common.Structures.HttpAuthentication/HttpAuthentication.cs
+++ Common.Structures.HttpAuthentication/HttpAuthentication.cs
@@ -1,7 +1,9 @@
-﻿using System.Net.Http.Headers;
+﻿using ProtoBuf;
+using System.Net.Http.Headers;
 
 namespace Common.Structures.HttpAuthentication
 {
+    [ProtoContract]
     public abstract class HttpAuthentication
     {
         public HttpAuthentication(string prefix, string value)
@@ -10,7 +12,10 @@ namespace Common.Structures.HttpAuthentication
             Value = value;
         }
 
+        [ProtoMember(1)]
         public string Prefix { get; set; }
+
+        [ProtoMember(2)]
         public string Value { get; set; }
 
         public AuthenticationHeaderValue Header

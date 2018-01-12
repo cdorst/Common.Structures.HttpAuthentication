e00d0ddd4b4da5565b0f65b7a4de92b96d6c5f82
Christopher Dorst <chris@dorst.email>
2018-01-12 16:54:04 -0600

    Make protected fields public properties

--- Common.Structures.HttpAuthentication/HttpAuthentication.cs
+++ Common.Structures.HttpAuthentication/HttpAuthentication.cs
@@ -4,16 +4,16 @@ namespace Common.Structures.HttpAuthentication
 {
     public abstract class HttpAuthentication
     {
-        protected readonly string _prefix;
-        protected readonly string _value;
-
         public HttpAuthentication(string prefix, string value)
         {
-            _prefix = prefix;
-            _value = value;
+            Prefix = prefix;
+            Value = value;
         }
 
+        public string Prefix { get; set; }
+        public string Value { get; set; }
+
         public AuthenticationHeaderValue Header
-            => new AuthenticationHeaderValue(_prefix, _value);
+            => new AuthenticationHeaderValue(Prefix, Value);
     }
 }
